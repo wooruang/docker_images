c5d7625126552f06bb63a0f2e2eea419effd95b1
wooruang <wooruang@gmail.com>
2019-05-10 14:44:27 +0900

    Add a dockerfile for a ssd of caffe

---
+++ caffe/ssd/ubuntu18.04-gpu/docker-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t wooruang/caffe-ssd:ubuntu18.04-cuda10.1-cudnn7 --file=Dockerfile .
---
+++ caffe/ssd/ubuntu18.04-gpu/docker-push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker login
+
+docker push wooruang/wooruang/caffe-ssd:ubuntu18.04-cuda10.1-cudnn7
