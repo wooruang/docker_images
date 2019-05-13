9ec8e82547fbbe32f05e45bc7f15796399116d73
wooruang <wooruang@gmail.com>
2019-05-13 15:01:41 +0900

    Change a version of cuda from 10.1 to 9.2 and a repository of caffe from weiliu to wooruang

--- caffe/ssd/ubuntu18.04-gpu/docker-build.sh
+++ caffe/ssd/ubuntu18.04-gpu/docker-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t wooruang/caffe-ssd:ubuntu18.04-cuda10.1-cudnn7 --file=Dockerfile .
+docker build -t wooruang/caffe-ssd:ubuntu18.04-cuda9.2-cudnn7 --file=Dockerfile .
--- caffe/ssd/ubuntu18.04-gpu/docker-push.sh
+++ caffe/ssd/ubuntu18.04-gpu/docker-push.sh
@@ -2,5 +2,5 @@
 
 docker login
 
-docker push wooruang/caffe-ssd:ubuntu18.04-cuda10.1-cudnn7
+docker push wooruang/caffe-ssd:ubuntu18.04-cuda9.2-cudnn7
 
