cf4c0a0c942b34950021e4a74aa448bd8fcb8c65
wooruang <wooruang@gmail.com>
2019-08-26 01:43:50 +0900

    Add a ssd of chuanqi305

---
+++ caffe/ssd-chuanqi305/ubuntu18.04-gpu/docker-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t wooruang/caffe-ssd-chuanqi305:ubuntu18.04-cuda10.1-cudnn7 --file=Dockerfile .
---
+++ caffe/ssd-chuanqi305/ubuntu18.04-gpu/docker-push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker login
+
+docker push wooruang/caffe-ssd-chuanqi305:ubuntu18.04-cuda10.1-cudnn7
+
