55814e709499420bbcbce25012072eafa9e69a6e
wooruang <wooruang@gmail.com>
2021-11-18 15:11:17 +0900

    Add for darknet

---
+++ darknet/cuda11.4.2/build-docker-api.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Build a docker image."
+echo "docker build -t bogonets/answer-api:${API_VERSION}-${TARGET_VERSION} --build-arg API_VERSION=${API_VERSION} --file=Dockerfile ."
+docker build -t wooruang/darknet:cuda11.4.2 .
